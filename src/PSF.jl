0ba9b8a01e7e20c30141be570c17b9d47a09f94e
Oleg Soloviev <oleg.soloviev@gmail.com>
2021-09-08 12:42:45 +0200

    fix for 1D PSF

--- src/PSF.jl
+++ src/PSF.jl
@@ -5,11 +5,11 @@ using FFTW
 field(amplitude, phase) = amplitude .* exp.( 1im * phase)
 
 "Calculate psf for given amplitude and phase"
-psf(field::Array) = abs.(toimageplane(field)) .^2
+psf(field::AbstractArray) = abs.(toimageplane(field)) .^2
 
 psf(amplitude, phase) = psf(field(amplitude, phase))
 
-subpsf(field::Array,Q::Integer) = psf(subdivide_sum(field,Q))
+subpsf(field::AbstractArray,Q::Integer) = psf(subdivide_sum(field,Q))
 subpsf(amplitude, phase,Q::Integer) = psf(subdivide_sum(field(amplitude, phase),Q))
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
 @testset "utils.jl" begin
     # Write your tests here.
     q1d = PhaseRetrieval.quadratic(5)
-    q2d = PhaseRetrieval.quadratic((5,5))
+    q2d = PhaseRetrieval.quadratic((5, 5))
     @test q1d == q2d ==  [
     8.0  5.0  4.0  5.0  8.0
     5.0  2.0  1.0  2.0  5.0
@@ -39,7 +39,7 @@ using Test
     #     NaN  NaN  NaN  NaN  NaN  NaN  NaN  NaN  NaN  NaN
     # ]    
 
-    @test PhaseRetrieval.lineararray(-1:.5:1, -1:.25:1, 3, 2,1) == [
+    @test PhaseRetrieval.lineararray(-1:.5:1, -1:.25:1, 3, 2, 1) == [
         -4.0  -2.5  -1.0  0.5  2.0
         -3.5  -2.0  -0.5  1.0  2.5
         -3.0  -1.5   0.0  1.5  3.0
@@ -59,13 +59,13 @@ using Test
         4.0  4.5  5.0  5.5  6.0
     ]
 
-    @test PhaseRetrieval.subdivide_sum(ones(10,10),5) == [
+    @test PhaseRetrieval.subdivide_sum(ones(10, 10), 5) == [
         25.0  25.0
         25.0  25.0
     ]
 
     a = SampledDomains.CartesianDomain2D(1:5, -2.5:.5:2.5)
-    ap = PhaseRetrieval.aperture(a, 2.5, (3,-0.1))
+    ap = PhaseRetrieval.aperture(a, 2.5, (3, -0.1))
     b = phwrap.(ap[2])
     # b  == [
     #     NaN  NaN    NaN    NaN    NaN
@@ -95,8 +95,26 @@ using Test
         1  1  1  1  1
     ]
 
-   a =  PhaseRetrieval.tile(reshape(1:60,(6,10)), 2)
+   a =  PhaseRetrieval.tile(reshape(1:60, (6, 10)), 2)
    @test a[:,:,1] == [ 1 7;2 8] && a[:,:,15] == [53 59; 54 60]
-   
+
+   a = 1:(4 * 3 * 3 * 3);
+   b = reshape(a, 4 * 3, 3 * 3);
+   c = PhaseRetrieval.subdivide(b, 3);
+   d = PhaseRetrieval.subdivide_sum(b,3);
+
+   @test  c[:,:,4] == [
+    37  49  61
+    38  50  62
+    39  51  63
+    40  52  64
+   ]
+
+   @test d == [
+    369  477  585
+    378  486  594
+    387  495  603
+    396  504  612
+   ]
 
 end
