1ecb93ffd98f0e60f4a1a849a48a6202abcfa10a
Oleg Soloviev <oleg.soloviev@gmail.com>
2022-05-23 17:58:19 +0200

    ui3860 added

--- src/hardware.jl
+++ src/hardware.jl
@@ -9,6 +9,7 @@ camerasdict["UI1490"] = PhaseRetrieval.CameraChip(pixelsize = 1.67E-3, imagesize
 camerasdict["UI1540"] = PhaseRetrieval.CameraChip(pixelsize = 5.2E-3, imagesize = (1280, 1024), bitdepth = 8, channelbitdepth = 8)
 camerasdict["UI2210"] = PhaseRetrieval.CameraChip(pixelsize = 9.9E-3, imagesize = (640, 480), bitdepth = 8, channelbitdepth = 8)
 camerasdict["UI3260"] = PhaseRetrieval.CameraChip(pixelsize = 5.86E-3, imagesize = (1936, 1216), bitdepth = 12, channelbitdepth = 16) #Bit depth of the images is 16
+camerasdict["UI3860"] = PhaseRetrieval.CameraChip(pixelsize = 2.9E-3, imagesize = (1936, 1096), bitdepth = 12, channelbitdepth = 16) #Bit depth of the images is 16
 
 # Lenses
 lensesdict["F300A25"] = PhaseRetrieval.ImagingLens(300, 25)
