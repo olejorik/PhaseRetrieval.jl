6efc4a9f28952d27c3de735b9e8327637a817d93
Oleg Soloviev <oleg.soloviev@gmail.com>
2022-10-13 12:31:34 +0200

    documentation bulid by github actions

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,37 @@
+using Pkg
+cd(@__DIR__)
+Pkg.activate(".")
+push!(LOAD_PATH,"../src/")
 using PhaseRetrieval
 using Documenter
+
+DocMeta.setdocmeta!(PhaseRetrieval, :DocTestSetup, :(using PhaseRetrieval); recursive=true)
+
 
 makedocs(;
     modules=[PhaseRetrieval],
     authors="Oleg Soloviev",
     repo="https://github.com/olejorik/PhaseRetrieval.jl/blob/{commit}{path}#L{line}",
     sitename="PhaseRetrieval.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://olejorik.github.io/PhaseRetrieval.jl",
-        assets=String[],
+    # format=Documenter.HTML(;
+        # prettyurls=get(ENV, "CI", "false") == "true",
+        # canonical="https://olejorik.github.io/PhaseRetrieval.jl",
+        # assets=String[],
+    # ),
+    format = Documenter.HTML(
+        # Use clean URLs, unless built as a "local" build
+        prettyurls = true,
+        canonical = "https://olejorik.github.io/PhaseRetrieval.jl/stable/",
+        # assets = ["assets/favicon.ico"],
+        # highlights = ["yaml"],
     ),
+    clean = false,
     pages=[
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/olejorik/PhaseRetrieval.jl",
+    repo="github.com/olejorik/PhaseRetrieval.jl.git",
+    target = "build",
 )
--- test/SHdiversityTest.jl
+++ test/SHdiversityTest.jl
@@ -20,11 +20,11 @@ heatmap(phwrap.(ph1), c=:cyclic_mygbm_30_95_c78_n256, size=(dim,dim)) |> display
 ap1 = [ i^2 + j^2 < r^2 for i in range(-1,1,length= dim), j in range(-1,1, length=dim)]
 display(heatmap(ap1))
 hp1ref = psf(ap1, ph1)
-heatmap(hp1ref, c=:grays, size = (540,540), cb=:none)
+heatmap(hp1ref, c=:grays, size = (540,540), cb=:none)  |> display
 
 # using Images instead of heatmap
-Gray.(PhaseRetrieval.rescale(phwrap.(ph1)))
-Gray.(PhaseRetrieval.rescale(hp1ref))
+Gray.(PhaseRetrieval.rescale(phwrap.(ph1))) |> display
+Gray.(PhaseRetrieval.rescale(hp1ref)) |> display
 
 # for 4 times more lenslets (32 x 32) we need upsample it 4 times
 dim=1080
@@ -32,12 +32,12 @@ q = 4
 dimup = dim*q
 phup = SHdiversity(dimup, cell, (12,-8.5), .1); # offset and rotation are chosen arbitrary
 phup = 2*pi*phup./(2* dimup)
-Gray.(PhaseRetrieval.rescale(phwrap.(phup)))
+Gray.(PhaseRetrieval.rescale(phwrap.(phup))) |> display
 apup = [ i^2 + j^2 < r^2 for i in range(-1,1,length= dimup), j in range(-1,1, length=dimup)]
 hpupref = imresize(psf(apup, phup*2), ratio=1/q)
-Gray.(PhaseRetrieval.rescale(hpupref))
+Gray.(PhaseRetrieval.rescale(hpupref)) |> display
 hp2=restrict(restrict(psf(apup, -phup*2)))
-Gray.(PhaseRetrieval.rescale(hp2))
+Gray.(PhaseRetrieval.rescale(hp2)) |> display
 
 # in MMA I had dimgrid 5850, q = 5, ap= 4805, number of pixel per s/a is 145
 dim=1170
@@ -47,8 +47,8 @@ r= 961/1170
 cellup =145
 phup = SHdiversity(dimup, cellup, (12,-8.5), .1); # offset and rotation are chosen arbitrary
 phup = 2*pi*phup./(2* dimup)
-Gray.(PhaseRetrieval.rescale(phwrap.(phup)))
+Gray.(PhaseRetrieval.rescale(phwrap.(phup))) |> display
 apup = [ i^2 + j^2 < r^2 for i in range(-1,1,length= dimup), j in range(-1,1, length=dimup)]
 hpupref = imresize(psf(apup, phup), ratio=1/q)
-Gray.(PhaseRetrieval.rescale(hpupref))
-save("p150f10_ref.png",Gray.(PhaseRetrieval.rescale(hpupref)))
+Gray.(PhaseRetrieval.rescale(hpupref)) |> display
+save("p150f10_ref.png",Gray.(PhaseRetrieval.rescale(hpupref))) 
